b6b0ecc2805adbee32b1e81202162c7ede9a2dcf
SnehaBarai1906 <baraisneha2003@gmail.com>
2025-07-14 22:06:20 +0530

    feat: initialize backend server with Express and CORS middleware

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app=express()
+const port=process.env.PORT || 4000
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+//api endpoints
+app.get('/', (req, res) => {
+  res.send('Welcome to the Seva API')
+});   
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
